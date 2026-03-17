7d28f2e67bc62a77341cad0240218d62f91b0f57
peshk0v <nikita.peshkovvv@gmail.com>
2026-03-17 16:59:40 +0300

    Use zapret-discord-youtube-linux

--- functions.py
+++ functions.py
@@ -130,37 +130,8 @@ def setObName(nm):
         f.write(nmm)
 
 def downloadzapret(config):
-    repo_url = 'https://api.github.com/repos/' + config["zapretDYRepo"] + '/releases/latest'
-    response = requests.get(repo_url)
-    
-    if response.status_code == 200:
-        latest_release = response.json()
-        zip_url = None
-        
-        for asset in latest_release.get("assets", []):
-            if asset["name"].endswith('.zip'):
-                zip_url = asset["browser_download_url"]
-                break
-        
-        if zip_url:
-            zip_response = requests.get(zip_url)
+    os.system("git clone https://github.com/Sergeydigl3/zapret-discord-youtube-linux.git data/")
+    os.system("./data/service.sh download-deps --default")
 
-            if zip_response.status_code == 200:
-                try:
-                    with zipfile.ZipFile(io.BytesIO(zip_response.content)) as z:
-                        z.extractall('data/')
-                    print("Скачивание и распаковка завершены.")
-                except zipfile.BadZipFile:
-                    print("Ошибка: полученный файл не является ZIP-архивом.")
-            else:
-                print(f'Ошибка при скачивании ZIP: {zip_response.status_code}')
-        else:
-            print("ZIP-архив не найден среди активов.")
-    else:
-        print(f'Ошибка при получении последнего релиза: {response.status_code}')
-    with open("/data/service.py", "w") as f:
-        with open("newservc.bat", "r") as s:
-            f.write(s.read()) 
-
-    applysets()
-    applyservc()
+#   applysets()
+#   applyservc()
